19a72396692cbf58e8512a3813acd8db774fbd8c
alliebrown8800 <91629048+alliebrown8800@users.noreply.github.com>
2021-10-20 20:32:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# Basic code for DC motor control using manual PWM
+# Set PWM period = 20 ms
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+pwmPin = 20
+GPIO.setup(pwmPin, GPIO.OUT)
+
+# set min & max % duty cycles
+dcMin = 0
+dcMax = 100
+
+pwm = GPIO.PWM(pwmPin, 50) # PWM object at 50 Hz (20 ms period)
+pwm.start(0)
+
+try:
+  while True:
+    for dc in range(dcMax,dcMin):
+      pwm.ChangeDutyCycle(dc)
+      print(dc)
+      time.sleep(.02)
+except KeyboardInterrupt:
+  print("closing")
+GPIO.cleanup()
